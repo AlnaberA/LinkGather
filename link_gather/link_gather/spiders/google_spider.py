906e7154a392f1ffe4fa946f4f6d9bcfe04df113
Echo <>
2021-01-30 23:19:04 -0500

    adding initial google spider(forgot to run git add .)

---
+++ link_gather/link_gather/spiders/google_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+def write_array_to_file(data):
+    with open("output.txt", "w") as txt_file:
+        for line in data:
+            txt_file.write(line)
+
+
+class GoogleSpider(scrapy.Spider):
+    name = "googleSpider"
+
+    start_urls = [
+        'http://www.google.com'
+    ]
+
+    def parse(self, response, **kwargs):
+        write_array_to_file(str(response.css("a::attr(href)").getall()))
